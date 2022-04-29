7cd512e82c74af4f551d86d3059c046f63d0a6e9
Evosmanay <104482334+Evosmanay@users.noreply.github.com>
2022-04-29 13:14:41 +0800

    Update viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -2,10 +2,10 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 ╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via DANA
-╠➥ *6281268477296*
+╠➥ *6285161697087*
 ║- Owner Bot:
-║- wa.me/6281268477296
-╠═〘 NEROHBOT 〙 ═
+║- wa.me/6285751516419
+╠═〘 DayatBot 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
